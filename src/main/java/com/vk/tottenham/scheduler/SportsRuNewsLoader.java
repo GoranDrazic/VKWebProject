4f6b53a7a9dde4002cf19d174f5021ffef25cb10
alexandr feskoff <v-afeskoff@expedia.com>
2017-02-28 14:55:09 +0300

    add news updaters

---
+++ src/main/java/com/vk/tottenham/scheduler/SportsRuNewsLoader.java
@@ -0,0 +1,59 @@
+package com.vk.tottenham.scheduler;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.vk.tottenham.core.model.Resource;
+import com.vk.tottenham.exception.VkException;
+import com.vk.tottenham.mybatis.service.ResourceService;
+
+public class SportsRuNewsLoader extends SchedulerBase {
+
+    private static final String URL = "http://www.sport.ru/football/tottenhem_angliya";
+    private static final String SHORT_URL = "http://www.sport.ru";
+
+    @Autowired
+    @Qualifier("resourceService")
+    private ResourceService newsService;
+
+    @Override
+    public void execute() {
+        try {
+            Document content = Jsoup.parse(new URL(URL), 10000);
+            Elements newsElements = content.getElementsByClass("articles-item-large");
+            for (Element newsElement : newsElements) {
+                Element newsLink = newsElement.getElementsByTag("h3").get(0)
+                        .getElementsByTag("a").get(0);
+                
+                String link = SHORT_URL + newsLink.attr("href").replaceAll("/$", "");
+                String title = newsLink.text();
+                
+                int lastSlash = link.lastIndexOf("/");
+                String id = link.substring(lastSlash + 1);
+                String longId = "news:" + "spurs_ru:" + id;
+                
+                if (!newsService.exists(longId)) {
+                    vkGateway
+                            .sendChatMessage("Sport.ru опубликовали новую статью \""
+                                    + title + "\"\n" + link, getChatId());
+                    Resource resource = new Resource();
+                    resource.setId(longId);
+                    newsService.save(resource);
+                    Thread.currentThread().sleep(5*1000);
+                }
+            }
+        } catch (IOException e) {
+            throw new VkException("Error loading spurs army news",e);
+        } catch (InterruptedException e) {
+            throw new VkException("Error loading spurs army news",e);
+        }
+    }
+
+}
---
+++ src/main/java/com/vk/tottenham/scheduler/SpursArmyNewsLoader.java
@@ -0,0 +1,58 @@
+package com.vk.tottenham.scheduler;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.vk.tottenham.core.model.Resource;
+import com.vk.tottenham.exception.VkException;
+import com.vk.tottenham.mybatis.service.ResourceService;
+
+public class SpursArmyNewsLoader extends SchedulerBase {
+
+    private static final String URL = "http://spursarmy.com/";
+
+    @Autowired
+    @Qualifier("resourceService")
+    private ResourceService newsService;
+
+    @Override
+    public void execute() {
+        try {
+            Document content = Jsoup.parse(new URL(URL), 10000);
+            Elements newsElements = content.getElementsByClass("textnews");
+            for (Element newsElement : newsElements) {
+                Element newsLink = newsElement.getElementsByClass("title").get(0)
+                        .getElementsByTag("a").get(0);
+                
+                String link = URL + newsLink.attr("href");
+                String title = newsLink.text();
+                
+                int lastSlash = link.lastIndexOf("/");
+                String id = link.substring(lastSlash + 1);
+                String longId = "news:" + "spurs_army:" + id;
+                
+                if (!newsService.exists(longId)) {
+                    vkGateway
+                            .sendChatMessage("SpursArmy опубликовали новую статью \""
+                                    + title + "\"\n" + link, getChatId());
+                    Resource resource = new Resource();
+                    resource.setId(longId);
+                    newsService.save(resource);
+                    Thread.currentThread().sleep(5*1000);
+                }
+            }
+        } catch (IOException e) {
+            throw new VkException("Error loading spurs army news",e);
+        } catch (InterruptedException e) {
+            throw new VkException("Error loading spurs army news",e);
+        }
+    }
+
+}
